d8fb056a61b70a408860b6c60a6dd47941380485
Nathaniel Wiley <nwiley86@gmail.com>
2017-12-26 10:37:23 -0500

    Add the initial JS

---
+++ auw3.js
@@ -0,0 +1,5 @@
+;var AUW3 = AUW3 || (function(){
+	document.body.addEventListener('click',function(event){
+		if(event.target.classList.contains('AUW3-closer')) event.target.parentElement.style.display='none';
+	});
+})();
