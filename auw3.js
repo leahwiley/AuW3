90a0ad0d470519e3bb6557744525d80d7814f7cf
Nathaniel Wiley <nwiley86@gmail.com>
2017-12-27 12:04:47 -0500

    IIFE refactor

--- auw3.js
+++ auw3.js
@@ -1,46 +1,63 @@
-/* AuW3.JS 0.1.0 December 2017 https://github.com/nathanielwiley/AuW3 */
+/* AuW3.JS 0.2.0 December 2017 https://github.com/nathanielwiley/AuW3 */
 /* Dependent on W3.JS https://www.w3schools.com/w3js/ by w3schools.com  */
 ;"use strict";
 if(typeof(w3) === 'object'){
-	var AuW3 = {
-		tasks:{
-			click:{hide:'',show:'','toggle-show':'',style:'','add-class':'','remove-class':'','toggle-class':''},
-			input:{'filter-html':''}
-		},
-		mapping:{
-			click:{hide:'hide',show:'show','toggle-show':'toggleShow',style:'addStyle','add-class':'addClass','remove-class':'removeClass','toggle-class':'toggleClass'},
-			input:{'filter-html':'filterHTML'}
-		},
-		useSimpleArgs:['hide','show','toggle-show'],
-		readTasks:function(event){
-			for(var i in AuW3.tasks[event.type]){
-				AuW3.tasks[event.type][i] = event.target.getAttribute('auw3-'+i);
+	var AuW3 = AuW3 || (function(){
+		var oTasks = {};
+		function buildTask(category,name,simpleArgs,map,useValue){
+			name = name || '';
+			map = map || name;
+			if(w3.hasOwnProperty(map)){
+				name = name.toLowerCase();
+				category = category || 'click';
+				category = category.toLowerCase();
+				simpleArgs = (typeof(simpleArgs) === 'boolean')? simpleArgs : false || false;
+				useValue = (typeof(useValue) === 'boolean')? useValue : false || false;
+				if(!oTasks.hasOwnProperty(category)) oTasks[category] = {};
+				oTasks[category][name] = { v:'', simple:simpleArgs, value:useValue , map:map };
 			}
-		},
-		runTasks:function(event){
-			AuW3.readTasks(event);
-			for(var i in AuW3.tasks[event.type]){
-				AuW3.runTask(event.type,i,event.target.value);
+		}
+		function readTasks(event){
+			for(var i in oTasks[event.type]){
+				oTasks[event.type][i].v = event.target.getAttribute('auw3-'+i);
 			}
-		},
-		runTask:function(category,name,value){
-			var taskQ = AuW3.tasks[category][name];
+		}
+		function runTask(category,name,value){
+			var thisTask = oTasks[category][name];
+			var taskQ = thisTask.v;
 			if(taskQ !== null && typeof(taskQ) === 'string'){
 				var taskA = taskQ.split(';');
 				for(var i in taskA){
 					var simpleArgs = taskA[i];
 					var complexArgs = simpleArgs.split(',');
-					if(AuW3.useSimpleArgs.indexOf(name) > -1){
-						w3[AuW3.mapping[category][name]](simpleArgs);
-					} else if(name === 'filter-html'){
-						w3[AuW3.mapping[category][name]](complexArgs[0],complexArgs[1],value);
+					if(thisTask.simple){
+						w3[thisTask.map](simpleArgs);
+					} else if(thisTask.value){
+						w3[thisTask.map](complexArgs[0],complexArgs[1],value);
 					} else {
-						w3[AuW3.mapping[category][name]](complexArgs[0],complexArgs[1],complexArgs[2]);
+						w3[thisTask.map](complexArgs[0],complexArgs[1],complexArgs[2]);
 					}
 				}
 			}
 		}
-	};
+		buildTask('input','filter-html',false,'filterHTML',true);
+		buildTask('click','hide',true);
+		buildTask('click','show',true);
+		buildTask('click','toggle-show',true,'toggleShow');
+		buildTask('click','style',false,'addStyle');
+		buildTask('click','add-class',false,'addClass');
+		buildTask('click','remove-class',false,'removeClass');
+		buildTask('click','toggle-class',false,'toggleClass');
+		return {
+			state:function(){ return oTasks; },
+			runTasks:function(event){
+				readTasks(event);
+				for(var i in oTasks[event.type]){
+					runTask(event.type,i,event.target.value);
+				}
+			}
+		};
+	})();
 	document.body.addEventListener('click',function(event){ AuW3.runTasks(event); });
 	document.body.addEventListener('input',function(event){ AuW3.runTasks(event); });
 }
